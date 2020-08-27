65489533f3dafb5e639dc25806091cac2d271da5
maqin <maqin@xinchao.com>
2020-08-27 15:14:19 +0800

    将ts版本转变为js版本

--- README.md
+++ README.md
@@ -7,3 +7,5 @@
 ##  2020/06/16 risk-assessment 中国风险评估（pc静态页面、vue-cli3及其生态）
 
 ##  2020/08/11 system 中联汽油田跟踪系统（pc图表大屏、vue-cli4 + ts + echarts）
+
+##  2020/08/24 system 中联汽油田跟踪系统（pc图表大屏、vue-cli4 + js + echarts）
---
+++ system-js/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Main />
+  </div>
+</template>
+
+<script>
+import Main from './Main.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Main
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+html, body {
+  font-size: calc(100vw / 13.66);
+  color: #333;
+}
+html, body, p, div, img {
+  margin: 0;
+  padding: 0;
+}
+</style>
